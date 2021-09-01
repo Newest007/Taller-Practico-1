40493bf9e0a06d6118b5ccfae5b0535458231e0b
Arian <adilsonarian@outlook.com>
2021-09-01 13:29:52 -0600

    Sexta versión-programacion del ejercicio 1 añadido y completo

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,6 +30,18 @@ namespace Taller_Practico_1
         private void InitializeComponent()
         {
             this.btnregresar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtresp2 = new System.Windows.Forms.TextBox();
+            this.txtresp1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnregresar
@@ -38,7 +50,7 @@ namespace Taller_Practico_1
             this.btnregresar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnregresar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnregresar.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnregresar.Location = new System.Drawing.Point(377, 247);
+            this.btnregresar.Location = new System.Drawing.Point(427, 237);
             this.btnregresar.Name = "btnregresar";
             this.btnregresar.Size = new System.Drawing.Size(125, 43);
             this.btnregresar.TabIndex = 14;
@@ -46,21 +58,167 @@ namespace Taller_Practico_1
             this.btnregresar.UseVisualStyleBackColor = false;
             this.btnregresar.Click += new System.EventHandler(this.btnejercicio1_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(39)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(228, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 43);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtresp2
+            // 
+            this.txtresp2.Location = new System.Drawing.Point(317, 151);
+            this.txtresp2.Name = "txtresp2";
+            this.txtresp2.Size = new System.Drawing.Size(108, 20);
+            this.txtresp2.TabIndex = 25;
+            this.txtresp2.TextChanged += new System.EventHandler(this.txtrespuesta2_TextChanged);
+            // 
+            // txtresp1
+            // 
+            this.txtresp1.Location = new System.Drawing.Point(131, 151);
+            this.txtresp1.Name = "txtresp1";
+            this.txtresp1.Size = new System.Drawing.Size(118, 20);
+            this.txtresp1.TabIndex = 24;
+            this.txtresp1.TextChanged += new System.EventHandler(this.txtrespuesta1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(288, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 19);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "X2";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(102, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 19);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "X1";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(456, 85);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(54, 20);
+            this.txtC.TabIndex = 21;
+            this.txtC.TextChanged += new System.EventHandler(this.txtC_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(285, 85);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(54, 20);
+            this.txtB.TabIndex = 20;
+            this.txtB.TextChanged += new System.EventHandler(this.txtB_TextChanged);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(107, 85);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(53, 20);
+            this.txtA.TabIndex = 19;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(380, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 23);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Valor de c";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Valor de a";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(208, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 23);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Valor de b";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 28);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Problema 3, Formula Cuadrática";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // frmejercicio1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 302);
+            this.ClientSize = new System.Drawing.Size(564, 292);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtresp2);
+            this.Controls.Add(this.txtresp1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnregresar);
             this.Name = "frmejercicio1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.frmejercicio1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnregresar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtresp2;
+        private System.Windows.Forms.TextBox txtresp1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -23,5 +23,99 @@ namespace Taller_Practico_1
             this.Close();
             frmprincipal.Visible = true;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            double valora, valorb, valorc, x1, x2, primeraparteformula;
+
+            valora = Convert.ToDouble(txtA.Text);
+            valorb = Convert.ToDouble(txtB.Text);
+            valorc = Convert.ToDouble(txtC.Text);
+            primeraparteformula = valorb * valorb - 4.0 * valora * valorc;
+
+            if (primeraparteformula < 0)
+            {
+                MessageBox.Show("El resultado es Imaginario", "");
+            }
+
+            else
+            {
+                x1 = (-valorb + Math.Sqrt(primeraparteformula)) / (2 * valora);
+                txtresp1.Text = x1.ToString();
+                x2 = (-valorb - Math.Sqrt(primeraparteformula)) / (2 * valora);
+                txtresp2.Text = x2.ToString();
+            }
+
+
+        }
+
+        private void txtrespuesta2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtrespuesta1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmejercicio1_Load(object sender, EventArgs e)
+        {
+            txtresp1.Enabled = false;
+            txtresp2.Enabled = false;
+
+        }
     }
 }
