ee158cf0e89323c947a44e7dcb1a91eb94410dfe
Arian Montes <89265906+Newest007@users.noreply.github.com>
2021-09-02 13:04:02 -0600

    Actualización de la rama "Ejercicio-2-y-3"
    Este commit y rama se ha hecho para actualizar archivos y arreglar problemas de compatibilidad entre la rama "Ejercicio-2-y-3" y la rama "Master", esta rama posee el ejercicio 3 y la interfaz del ejercicio 2

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -30,6 +30,23 @@ namespace Taller_Practico_1
         private void InitializeComponent()
         {
             this.btnregresar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtboxnum1 = new System.Windows.Forms.TextBox();
+            this.txtboxnum2 = new System.Windows.Forms.TextBox();
+            this.txtboxnum3 = new System.Windows.Forms.TextBox();
+            this.txtboxnum4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtboxmay = new System.Windows.Forms.TextBox();
+            this.txtboxmen = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnregresar
@@ -46,21 +63,194 @@ namespace Taller_Practico_1
             this.btnregresar.UseVisualStyleBackColor = false;
             this.btnregresar.Click += new System.EventHandler(this.btnregresar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Número 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Número 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Número 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(204, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "CALCULO MAYOR Y MENOR";
+            // 
+            // txtboxnum1
+            // 
+            this.txtboxnum1.Location = new System.Drawing.Point(100, 94);
+            this.txtboxnum1.Name = "txtboxnum1";
+            this.txtboxnum1.Size = new System.Drawing.Size(40, 20);
+            this.txtboxnum1.TabIndex = 22;
+            // 
+            // txtboxnum2
+            // 
+            this.txtboxnum2.Location = new System.Drawing.Point(216, 94);
+            this.txtboxnum2.Name = "txtboxnum2";
+            this.txtboxnum2.Size = new System.Drawing.Size(39, 20);
+            this.txtboxnum2.TabIndex = 23;
+            // 
+            // txtboxnum3
+            // 
+            this.txtboxnum3.Location = new System.Drawing.Point(333, 94);
+            this.txtboxnum3.Name = "txtboxnum3";
+            this.txtboxnum3.Size = new System.Drawing.Size(39, 20);
+            this.txtboxnum3.TabIndex = 24;
+            // 
+            // txtboxnum4
+            // 
+            this.txtboxnum4.Location = new System.Drawing.Point(453, 94);
+            this.txtboxnum4.Name = "txtboxnum4";
+            this.txtboxnum4.Size = new System.Drawing.Size(40, 20);
+            this.txtboxnum4.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Ingrese los números:";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btncalcular.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.ForeColor = System.Drawing.SystemColors.Control;
+            this.btncalcular.Location = new System.Drawing.Point(355, 301);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(104, 43);
+            this.btncalcular.TabIndex = 16;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Número Mayor";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(277, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Número Menor";
+            // 
+            // txtboxmay
+            // 
+            this.txtboxmay.Location = new System.Drawing.Point(97, 64);
+            this.txtboxmay.Name = "txtboxmay";
+            this.txtboxmay.Size = new System.Drawing.Size(100, 20);
+            this.txtboxmay.TabIndex = 29;
+            // 
+            // txtboxmen
+            // 
+            this.txtboxmen.Location = new System.Drawing.Point(360, 64);
+            this.txtboxmen.Name = "txtboxmen";
+            this.txtboxmen.Size = new System.Drawing.Size(100, 20);
+            this.txtboxmen.TabIndex = 30;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtboxmen);
+            this.groupBox1.Controls.Add(this.txtboxmay);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(44, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(541, 145);
+            this.groupBox1.TabIndex = 31;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultados";
+            // 
             // frmejercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(611, 356);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtboxnum4);
+            this.Controls.Add(this.txtboxnum3);
+            this.Controls.Add(this.txtboxnum2);
+            this.Controls.Add(this.txtboxnum1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnregresar);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmejercicio3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form4";
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnregresar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtboxnum1;
+        private System.Windows.Forms.TextBox txtboxnum2;
+        private System.Windows.Forms.TextBox txtboxnum3;
+        private System.Windows.Forms.TextBox txtboxnum4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtboxmay;
+        private System.Windows.Forms.TextBox txtboxmen;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
